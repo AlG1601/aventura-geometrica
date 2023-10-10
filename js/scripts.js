dedda0a41d58ccba88f339943b97fda3bb8aaddd
Guilherme Alefe de Sousa dos Santos <guilherme.gui1601@gmail.com>
2023-10-10 18:21:14 -0300

    create icons

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 /*!
-* Start Bootstrap - Freelancer v7.0.7 (https://startbootstrap.com/theme/freelancer)
+* Start Bootstrap - Freelancer v7.0.7 (https://startbotstrap.com/theme/freelancer)
 * Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
+* Licensed under MIT (https://github.com/StartBotstrap/startbootstrap-freelancer/blob/master/LICENSE)
 */
 //
 // Scripts
